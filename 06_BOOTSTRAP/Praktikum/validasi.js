f47fa4f11d76179f3202f2f83cf2daa8726df6ae
Nandar Sulistiawan <nandarsulistiawan21@mhs.unsiq.ac.id>
2023-08-29 22:18:47 +0700

    Update Praktikum dan Screenshot

---
+++ 06_BOOTSTRAP/Praktikum/validasi.js
@@ -0,0 +1,20 @@
+(() => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".account");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
