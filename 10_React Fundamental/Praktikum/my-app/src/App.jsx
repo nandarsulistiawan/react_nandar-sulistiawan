7e58170f60bb69694d28bf1a99db880bd02e5bc5
Nandar Sulistiawan <nandarsulistiawan21@mhs.unsiq.ac.id>
2023-09-24 01:29:13 +0700

    menambahkan Routes dan Route pada App.jsx

--- 10_React Fundamental/Praktikum/my-app/src/App.jsx
+++ 10_React Fundamental/Praktikum/my-app/src/App.jsx
@@ -1,13 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import NavbarComponent from "./components/NavbarComponent";
-import TittleComponent from "./components/TittleComponent";
+import LandingPage from "./pages/LandingPage";
+import CreateAccount from "./pages/CreateAccount";
 import CreateProduct from "./pages/CreateProduct";
 
 function App() {
   return (
     <>
       <NavbarComponent />
-      <TittleComponent/>
-      <CreateProduct/>
+      <Routes>
+        <Route path="/" Component={LandingPage} />
+        <Route path="/createaccount" Component={CreateAccount} />
+        <Route path="/createproduct" Component={CreateProduct} />
+      </Routes>
     </>
   );
 }
