740182b993c77596dde526d0e3ea41f3bca2266c
Nandar Sulistiawan <nandarsulistiawan21@mhs.unsiq.ac.id>
2023-10-01 08:34:32 +0700

    menghapus style css

--- 10_React Fundamental/Praktikum/my-app/src/components/TittleComponent.jsx
+++ 10_React Fundamental/Praktikum/my-app/src/components/TittleComponent.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Button, Container, Row, Col } from "react-bootstrap";
 import bootstrapLogo from "../assets/bootstrap-logo.svg";
-import "../createproduct.css";
 
 const TittleComponent = () => {
   const [language, setLanguage] = useState("english");
