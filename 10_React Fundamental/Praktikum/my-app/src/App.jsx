ccdb64f472fb80cd791f12e7a7d7df1edef7dc88
Nandar Sulistiawan <nandarsulistiawan21@mhs.unsiq.ac.id>
2023-10-01 08:32:06 +0700

    Upadate Route DetailProduct dan ListProduct

--- 10_React Fundamental/Praktikum/my-app/src/App.jsx
+++ 10_React Fundamental/Praktikum/my-app/src/App.jsx
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import NavbarComponent from "./components/NavbarComponent";
 import LandingPage from "./pages/LandingPage";
-import CreateAccount from "./pages/CreateAccount";
 import CreateProduct from "./pages/CreateProduct";
+import DetailProduct from "./pages/DetailProduct";
+import { ListProduct } from "./pages/ListProduct";
+import CreateAccount from "./pages/CreateAccount";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Routes>
         <Route path="/" Component={LandingPage} />
         <Route path="/createaccount" Component={CreateAccount} />
+        <Route path="/listproduct" Component={ListProduct} />
         <Route path="/createproduct" Component={CreateProduct} />
+        <Route path="/detailproduct/:id" Component={DetailProduct} />
       </Routes>
     </>
   );
