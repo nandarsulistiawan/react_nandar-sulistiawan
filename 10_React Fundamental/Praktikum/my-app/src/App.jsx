789cc2939fd03ba7ca6ddcd5f41d14656928cdfa
Nandar Sulistiawan <nandarsulistiawan21@mhs.unsiq.ac.id>
2023-10-19 05:48:48 +0700

    menambahkan route AiPage

--- 10_React Fundamental/Praktikum/my-app/src/App.jsx
+++ 10_React Fundamental/Praktikum/my-app/src/App.jsx
@@ -5,6 +5,7 @@ import CreateProduct from "./pages/CreateProduct";
 import DetailProduct from "./pages/DetailProduct";
 import { ListProduct } from "./pages/ListProduct";
 import CreateAccount from "./pages/CreateAccount";
+import AiPage from "./pages/AiPage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/listproduct" Component={ListProduct} />
         <Route path="/createproduct" Component={CreateProduct} />
         <Route path="/detailproduct/:id" Component={DetailProduct} />
+        <Route path="/aipage" element={<AiPage />} />
       </Routes>
     </>
   );
